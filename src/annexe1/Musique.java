3b82f73a63fba1581ca8113d4dd17c2a16109e83
DamienCPE <dami184@live.fr>
2014-09-26 08:19:10 +0200

    add Musique

---
+++ src/annexe1/Musique.java
@@ -0,0 +1,28 @@
+package annexe1;
+
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Track;
+
+public class Musique {
+	private Sequence seq;
+	private Track piste;
+	
+	public Sequence getSeq() {
+		return seq;
+	}
+	public void setSeq(Sequence seq) {
+		this.seq = seq;
+	}
+	public Track getPiste() {
+		return piste;
+	}
+	public void setPiste(Track piste) {
+		this.piste = piste;
+	}
+	
+	public Musique() {
+		super();
+	}
+	
+	
+}
