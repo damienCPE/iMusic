0ef6ce4fb9ad193bf6a8fde99576065238a6fcad
DamienCPE <dami184@live.fr>
2014-10-06 10:56:32 +0200

    add params to creationFigure methode

--- src/partie2/model/FigureOval.java
+++ src/partie2/model/FigureOval.java
@@ -4,8 +4,8 @@ import java.awt.Graphics;
 
 public class FigureOval extends Figure{
 	@Override
-	public Graphics creationFigure() {
-		super.creationFigure();
+	public Graphics creationFigure(int maxHeight, int maxWidth, int height, int width) {
+		super.creationFigure(maxHeight, maxWidth, height, width);
 		this.graphic.fillOval(this.x,this.y,this.ht, this.width);
 		return this.graphic;
 	}
