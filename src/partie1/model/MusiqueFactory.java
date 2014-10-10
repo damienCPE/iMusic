535b650cc95f3ff8ec22ce3ef07098a44828689c
DamienCPE <dami184@live.fr>
2014-10-10 11:27:33 +0200

    create a musique with server information

--- src/partie1/model/MusiqueFactory.java
+++ src/partie1/model/MusiqueFactory.java
@@ -3,28 +3,30 @@ package partie1.model;
 import partie3.model.MusiqueFichierServer;
 
 public class MusiqueFactory {
-	
-    public MusiqueFactory(){}
-    
-    // Creer l'objet musique en fonction du type et du chemin du fichier passe en parametre
-    public Musique creationMusique(TypeMusique type, String s){
-        Musique musique = null;
-        switch (type) {
-        
-        case FICHIER:
-            if(s.compareTo("") == 0){
-                return null;
-            }
-            musique = new MusiqueFichier(s);
-            break;
-        case GENEREE:
-            musique = new MusiqueGeneree();
-            break;
-	    case SERVER:
-	        musique = new MusiqueFichierServer(s);
-	        break;
-	    }
-        return musique;
-    }
-	
+
+	public MusiqueFactory() {
+	}
+
+	// Creer l'objet musique en fonction du type et du chemin du fichier passe
+	// en parametre
+	public Musique creationMusique(TypeMusique type, String s) {
+		Musique musique = null;
+		switch (type) {
+
+		case FICHIER:
+			if (s.compareTo("") == 0) {
+				return null;
+			}
+			musique = new MusiqueFichier(s);
+			break;
+		case GENEREE:
+			musique = new MusiqueGeneree();
+			break;
+		case SERVER:
+			musique = new MusiqueFichierServer(s);
+			break;
+		}
+		return musique;
+	}
+
 }
--- src/partie3/model/MusiqueFichierServer.java
+++ src/partie3/model/MusiqueFichierServer.java
@@ -1,35 +1,48 @@
 package partie3.model;
 
 import java.io.File;
+import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
-import partie1.model.Musique;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Track;
 
-public class MusiqueFichierServer extends Musique{ 
+import partie1.model.Musique;
+import partie3.serveur.IServeurRMI;
+
+public class MusiqueFichierServer extends Musique {
 	String chemin;
-	
-	public MusiqueFichierServer(String s){
-		/*this.chemin = s;
-		ServiceInterface stub;
-		try
-		{
+	private IServeurRMI stub;
+
+	public MusiqueFichierServer(String s) {
+		this.chemin = s;
+		IServeurRMI stub;
+		try {
 			// Initialisation du registry sur le port 1099
 			Registry registry = LocateRegistry.getRegistry(1099);
-			
+
 			// Récupération du stub lié au serveur
-			this.stub = (ServiceInterface) registry.lookup("MusicService");
+			this.stub = (IServeurRMI) registry.lookup("MusicService");
+		} catch (Exception e) {
+
 		}
-		catch (Exception e) {
-			
-		}*/
-		
+
 	}
 
 	@Override
 	public int creationPiste() {
-		// TODO Auto-generated method stub
+		// creer une sequence et une piste
+		try {
+			chemin = this.stub.getInformation();
+			this.setSeq(MidiSystem.getSequence(new File(chemin)));
+		} catch (Exception e) {
+			return -1;
+		}
+
+		// Sequence seq = new Sequence(Sequence.PPQ, 4);
+		Track piste = this.getSeq().createTrack();
+
 		return 0;
 	}
-	
 
 }
