a4a67b75265acbd34a786f7ac676519144bb5b6f
DamienCPE <dami184@live.fr>
2014-10-10 16:52:07 +0200

    resolve serveur issu

--- src/partie3/serveur/MusicService.java
+++ src/partie3/serveur/MusicService.java
@@ -8,24 +8,8 @@ import java.rmi.server.RMIClientSocketFactory;
 import java.rmi.server.RMIServerSocketFactory;
 import java.rmi.server.UnicastRemoteObject;
 
-public class MusicService extends UnicastRemoteObject implements IService {
-
-	protected MusicService() throws RemoteException {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	protected MusicService(int port) throws RemoteException {
-		super(port);
-		// TODO Auto-generated constructor stub
-	}
-
-	protected MusicService(int port, RMIClientSocketFactory csf,
-			RMIServerSocketFactory ssf) throws RemoteException {
-		super(port, csf, ssf);
-		// TODO Auto-generated constructor stub
-	}
-
+public class MusicService implements IService {
+	
 	private static final long serialVersionUID = 2674880711467464646L;
 
 	/*public String getInformation() throws RemoteException {
