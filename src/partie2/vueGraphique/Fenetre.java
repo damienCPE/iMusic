c2a5fad17e7bd724077b26f3455b4cce9212f38e
Jérémy Longin <jeremy.longin01@gmail.com>
2014-10-10 17:05:05 +0200

    Change size

--- src/partie2/vueGraphique/Fenetre.java
+++ src/partie2/vueGraphique/Fenetre.java
@@ -13,7 +13,7 @@ public class Fenetre extends JFrame {
         super();
 
         this.setTitle("IMusic v2.0");
-        this.setSize(1200, 800);
+        this.setSize(1400, 800);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setResizable(false);
         this.setLayout(null);
