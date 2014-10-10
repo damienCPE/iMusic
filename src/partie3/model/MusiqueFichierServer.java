e9eed42a5659001303072e1fa846d8c12d2f0b29
DamienCPE <dami184@live.fr>
2014-10-10 13:51:20 +0200

    change the interface

--- src/partie3/model/MusiqueFichierServer.java
+++ src/partie3/model/MusiqueFichierServer.java
@@ -3,26 +3,24 @@ package partie3.model;
 import java.io.File;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.Track;
-
 import partie1.model.Musique;
-import partie3.serveur.IServeurRMI;
+import partie3.serveur.IService;
 
 public class MusiqueFichierServer extends Musique {
 	String chemin;
-	private IServeurRMI stub;
+	private IService stub;
 
 	public MusiqueFichierServer(String s) {
 		this.chemin = s;
-		IServeurRMI stub;
+		IService stub;
 		try {
 			// Initialisation du registry sur le port 1099
 			Registry registry = LocateRegistry.getRegistry(1099);
 
 			// Récupération du stub lié au serveur
-			this.stub = (IServeurRMI) registry.lookup("MusicService");
+			this.stub = (IService) registry.lookup("MusicService");
 		} catch (Exception e) {
 
 		}
