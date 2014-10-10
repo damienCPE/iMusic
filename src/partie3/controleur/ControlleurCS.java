5a398b93761f086358f4e90d2f345c238a9f910c
DamienCPE <dami184@live.fr>
2014-10-10 16:25:12 +0200

    change the controlleur method chargerFichier

--- src/partie3/controleur/ControlleurCS.java
+++ src/partie3/controleur/ControlleurCS.java
@@ -1,19 +1,22 @@
 package partie3.controleur;
 
 import partie1.controleur.Controlleur;
+import partie3.model.PlayerCS;
 
 public class ControlleurCS extends Controlleur {
 	protected Controlleur controlleur;
-
+	protected PlayerCS playercs;
+	
 	public ControlleurCS() {
 		super();
 		this.controlleur = new Controlleur();
+		this.playercs = new PlayerCS();
 	}
 	
     // Charge le fichier midi passer en parametre
-    public int chargerFichier(String f) {
-        this.player.chargerFichier(f);
-        this.musList.makeEvent(this.player);
+    public int chargerFichierServeur(String f) {
+        this.playercs.chargerFichier(f);
+        this.musList.makeEvent(this.playercs);
         return 0;
     }
 }
